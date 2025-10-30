74800d78f5f62aaf204fac4a42dfa447e3468b00
nymerys <jeaniechen22@gmail.com>
2025-10-30 15:29:29 +0900

    created playround function and incremented score variables

--- script.js
+++ script.js
@@ -21,10 +21,9 @@ console.log(computerResult);
 
 function getHumanChoice() {
     let playerResult = prompt("What would you like to throw? Rock, paper, scissors!");
-    console.log(playerResult);
+    console.log(playerResult.toLowerCase());
+    return playerResult;
 }
-
-getHumanChoice();
 
 /**
  * Write a prompt to instruct the player to input a value.
@@ -33,26 +32,62 @@ getHumanChoice();
  */
 
 let humanScore = 0;
+const humanScoreCount = humanScore++;
 
 let computerScore = 0;
+const computerScoreCount = computerScore++;
+
 // Assign a variable to log the player's score.
 // Assign a variable to log the computer's score.
-
+// Initialize both variables.
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice + computerChoice;
-  // your code here!
+    if (humanChoice === "rock" && computerChoice === "scissors") return "You win! Rock beats scissors.";
+    if (humanChoice === "rock" && computerChoice === "paper") return "You lose! Paper beats rock.";
+    if (humanChoice === "rock" && computerChoice === "rock") return "It's a tie!";
+    if (humanChoice === "paper" && computerChoice === "rock") return "You win! Paper beats rock.";
+    if (humanChoice === "paper" && computerChoice === "scissors") return "You lose! Scissors beats paper.";
+    if (humanChoice === "paper" && computerChoice === "paper") return "It's a tie!";
+    if (humanChoice === "scissors" && computerChoice === "paper") return "You win! Scissors beats paper.";
+    if (humanChoice === "scissors" && computerChoice === "rock") return "You lose! Rock beats scissors.";
+    if (humanChoice === "scissors" && computerChoice === "scissors") return "It's a tie!";
 }
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(throwNumber);
 
-// playRound(humanSelection, computerSelection);
+let roundResult = playRound(humanSelection, computerSelection);
+console.log(roundResult);
 
-
-function sum(firstNumber, secondNumber) {
-    let sum = firstNumber + secondNumber;
-    console.log(sum)
+function humanWin(roundResult) {
+    if (roundResult.includes("You win!")) return humanScore;
+    else return computerScoreCount;
 }
 
-sum(3,2);
+function computerWin(roundResult) {
+    if (roundResult.includes("You lose!")) return computerScore;
+    else return humanScoreCount;
+}
+
+let humanPoints = humanWin(roundResult);
+console.log(humanPoints);
+
+let computerPoints = computerWin(roundResult);
+console.log(computerPoints);
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice.
+// Use these two parameters to take the human and computer choices as arguments.
+// Make humanChoice parameter case-insensitive.
+// playRound function to console.log a string value to indicate round winner.
+// Increment the humanScore or computerScore variable based on the round winner.
+
+function playGame(){
+    
+}
+
+// Create a new function named playGame.
+// Move your playRound function and score variables so that they’re declared inside of the new playGame function
+// Play 5 rounds by calling playRound 5 times.
+// Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
+// Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
